5310350b0022e6c5d67437d5248eb6fc7facc31a
Candra Kriswinarto <ckriswinarto@gmail.com>
2019-11-14 16:36:18 +0800

    Add message when user add or remove task with different style color

--- src/App.js
+++ src/App.js
@@ -7,28 +7,49 @@ import './App.css'
 
 class App extends Component {
   state = {
-    todos: []
+    todos: [],
+    message: '',
+    classMessage: ''
   }
   deleteTodo = (id) => {
     const todos = this.state.todos.filter(todo => {
       return todo.id !== id
     });
     this.setState({
-      todos: todos
+      todos: todos,
+      message: "Task has been remove successfully",
+      classMessage: "danger"
+
     })
+    setTimeout(() => {
+      this.setState({
+        message: ""      })
+    }, 2000)
   }
   addTodo = (todo) => {
     todo.id = Math.random();
     let todos = [...this.state.todos, todo];
     this.setState({
-      todos: todos
+      todos: todos,
+      message: "New todo has been added successfully",
+      classMessage: "success"
     })
+    setTimeout(() => {
+      this.setState({
+        message: ""      })
+    }, 2000)
   }
 
   render() {
+      const message = this.state.message !== '' ? (
+        <div className={`message ${this.state.classMessage}`}>
+          <p>{this.state.message}</p>
+        </div>
+      ) : null;
     return (
       <div className="app">
         <h1>My Todos App</h1>
+        {message}
         <Todos  deleteTodo={this.deleteTodo} todos={this.state.todos} />
         <AddTodo addTodo={this.addTodo} />
       </div>
